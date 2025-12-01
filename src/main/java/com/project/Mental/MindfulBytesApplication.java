bb8aac59a78ed22a8e5ec466f4ccbba60fad1bad
Loai Al-Qadasi <LoaialQadasi2@gmail.com>
2025-12-01 14:58:10 +0800

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/project/Mental/MindfulBytesApplication.java
@@ -0,0 +1,13 @@
+package com.project.Mental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindfulBytesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindfulBytesApplication.class, args);
+	}
+
+}
