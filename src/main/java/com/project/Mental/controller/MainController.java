f224cbf51f3d033451437da1741f14c07497609e
MOHAMED ABDELGAWWAD <166241569+MoAbdo122004@users.noreply.github.com>
2026-01-13 15:29:35 +0800

    Update MainController.java

--- src/main/java/com/project/Mental/controller/MainController.java
+++ src/main/java/com/project/Mental/controller/MainController.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.project.Mental.controller;
 
 import org.springframework.stereotype.Controller;
@@ -66,26 +65,3 @@ public class MainController {
     public String showLogin() {
         return "login";
     }
-=======
-package com.project.Mental.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class MainController {
-
-    // This handles requests to "localhost:8080/" (The landing page)
-    @GetMapping("/")
-    public String index() {
-        return "index"; // You might need an index.html later
-    }
-
-    // This handles "localhost:8080/dashboard"
-    @GetMapping("/dashboard")
-    public String showDashboard() {
-        // This looks for src/main/resources/templates/student/dashboard.html
-        return "student/dashboard";
-    }
->>>>>>> 6f0699dff822453932d99aa7519157956dace76b
-}
